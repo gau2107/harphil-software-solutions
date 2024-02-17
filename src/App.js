3a134397b3dc010ffc7d469b68c785ed6d5cb51c
Solanki Gaurav <solankigaurav2107@gmail.com>
2024-02-17 13:26:44 +0530

    initialize route

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 export default function App() {
   return (
-    <></>
+    <Router>
+      <Routes>
+        <Route path="/" element={<span>Home</span>} />
+        <Route path="/character/:id" element={<span>Character</span>} />
+      </Routes>
+    </Router>
   )
 }
